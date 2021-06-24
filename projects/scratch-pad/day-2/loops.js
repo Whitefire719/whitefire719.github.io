d6d7f05be61ee0a1519d6f79a3cb218aa096a472
Ubuntu <ubuntu@ip-172-31-54-83.ec2.internal>
2021-06-24 19:29:08 +0000

    Updated work

---
+++ projects/scratch-pad/day-2/loops.js
@@ -0,0 +1,170 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: LOOPS
+ */
+
+/** 
+ * Given an input Array, loop forward over the Array and print its values 
+ * using console.log().
+ */
+function printArrayValues(array) {
+  // YOUR CODE BELOW HERE //
+var dataArray = array;
+ for (let i = 0; i <= dataArray.length; i++) {
+   console.log(dataArray[i]);
+ }
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Array, loop backwards over the Array and print its values 
+ * using console.log().
+ */
+function printArrayValuesInReverse(array) {
+  // YOUR CODE BELOW HERE //
+  var reverseArray = array;
+ for (let i = reverseArray.length - 1; i >= 0; i--) {
+   console.log(reverseArray[i]);
+ }
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, return an Array containing the Object keys.
+ */
+function getObjectKeys(object) {
+  // YOUR CODE BELOW HERE //
+var keyArray = object;
+
+return Object.keys(keyArray);
+}
+
+  
+
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+
+
+/** 
+ * Given an input Object, loop over the Object and print its keys 
+ * using console.log().
+ */
+function printObjectKeys(object) {
+  // YOUR CODE BELOW HERE //
+var objectKeys = object;
+  for (var key in objectKeys) {
+    if(objectKeys.hasOwnProperty(key)) {
+      console.log(key, objectKeys[key]);
+    } 
+  }  
+  
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, return an Array containing the Object's values.
+ */
+function getObjectValues(object) {
+  // YOUR CODE BELOW HERE //
+  var valueArray = object;
+  return Object.values(valueArray);
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, loop over the Object and print its values 
+ * using console.log().
+ */
+function printObjectValues(object) {
+  // YOUR CODE BELOW HERE //
+  var objectValues = object;
+  for (var value in objectValues) {
+    if(objectValues.hasOwnProperty(value)) {
+      console.log(objectValues[value]);
+    }
+  }
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, return the number of key/value pairs stored within that Object.
+ */
+function getObjectLength(object) {
+  // YOUR CODE BELOW HERE //
+  var keyValuePairs = object;
+  return Object.values(keyValuePairs).length;
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, how might we loop over the Object IN REVERSE and 
+ * print its values using console.log()?
+ */
+function printObjectValuesInReverse(object) {
+  // YOUR CODE BELOW HERE //
+var reverseVal = Object.values(object);
+ for(var x = reverseVal.length - 1; x >= 0; x--) {
+  console.log(reverseVal[x]);
+  
+}}
+  
+
+
+
+
+
+  {
+
+ 
+  
+  
+ 
+    
+  
+  
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.printArrayValues = printArrayValues;
+    module.exports.printArrayValuesInReverse = printArrayValuesInReverse;
+    module.exports.printObjectValues = printObjectValues;
+    module.exports.getObjectValues = getObjectValues;
+    module.exports.getObjectKeys = getObjectKeys;
+    module.exports.printObjectKeys = printObjectKeys;
+    module.exports.getObjectLength = getObjectLength;
+    module.exports.printObjectValuesInReverse = printObjectValuesInReverse;
+}
---
+++ projects/scratch-pad/day-3/first-class-functions.js
@@ -0,0 +1,171 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
+ */
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is greater than the 
+ * base.
+ */
+function createGreaterThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+// function checkStuff(base) {
+//     return base < 1;
+// } return checkStuff(base);
+//     } 
+function greaterBase(value) {
+    return value > base;
+}
+ 
+return greaterBase;
+
+
+
+    
+
+    
+    
+    
+    
+
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is less than the 
+ * base.
+ */
+function createLessThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    function lesserBase(value) {
+    return value < base;
+}
+ 
+return lesserBase;
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a startsWith character, which will be a single character, return a 
+ * Function that tests whether a given String starts with the startsWith 
+ * character.
+ */
+function createStartsWithFilter(startsWith) {
+    // YOUR CODE BELOW HERE //
+    function beginsWith(string) {
+        
+        var firstLetter = string[0];
+        return firstLetter.toLowerCase() === startsWith.toLowerCase();
+          
+        
+    } 
+    return beginsWith;
+    
+
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a endsWith character, which will be a single character, return a 
+ * Function that tests whether a given String ends with the endsWith 
+ * character.
+ */
+function createEndsWithFilter(endsWith) {
+    // YOUR CODE BELOW HERE //
+    function resolvesWith(string) {
+        
+        var lastLetter = string[string.length - 1];
+        return lastLetter.toLowerCase() === endsWith.toLowerCase();
+          
+        
+    } 
+    return resolvesWith;
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to modify a String, 
+ * return the Array of the Strings, modified.
+ * 
+ * TIP: You need to loop over the Strings, right? We need to pass each String to 
+ * the modify Function, but we need to collect the results into some collection.
+ */
+function modifyStrings(strings, modify) {
+    // YOUR CODE BELOW HERE //
+    // func as param
+    var result = [];
+    for (var i = 0; i < strings.length; i++) {
+        var string = strings[i];
+        result.push(modify(string));
+    }
+    return result;
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to test the String in some 
+ * way and return a Boolean on whether it passed, return true if ALL Strings pass the test.
+ * 
+ * Imagine you had a list of names, and you wanted to test they all 
+ * begin with "C", or they are all exclaimations that end with "!".
+ * 
+ * TIP: You need to loop over the Strings, right? And pass them to the test?
+ */
+function allStringsPass(strings, test) {
+    // YOUR CODE BELOW HERE //
+    // func as param
+        var arr = [];
+    for (var i = 0; i <= strings.length - 1; i++) {
+        var string = strings[i];
+       
+        arr.push(string);
+        if(test(strings[i]))  {
+            return true;
+        } else return false;
+       
+       
+       
+       
+        
+     
+     
+     
+    
+        
+    }
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.createGreaterThanFilter = createGreaterThanFilter;
+    module.exports.createLessThanFilter = createLessThanFilter;
+    module.exports.createStartsWithFilter = createStartsWithFilter;
+    module.exports.createEndsWithFilter = createEndsWithFilter;
+    module.exports.modifyStrings = modifyStrings;
+    module.exports.allStringsPass = allStringsPass;   
+}
